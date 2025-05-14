8b4f07b11ad9eb98db344e9d2bd841a36bf6a570
sahaj <sahaj@Admins-MacBook-Air-2.local>
2025-05-14 18:21:57 +0530

    d

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["books.toscrape.com"] },
-  headers: async () => {
-    return process.env.VERCEL_ENV === "production"
-      ? [] // ✅ allow indexing
-      : [
-          {
-            source: "/(.*)",
-            headers: [
-              {
-                key: "X-Robots-Tag",
-                value: "noindex", // ❌ only for preview or dev
-              },
-            ],
-          },
-        ];
-  },
 };
 
 module.exports = nextConfig;
